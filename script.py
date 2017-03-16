c05a03ea18e7104dac016568a4920d8e0f290c99
rahul2992 <rahuljainnsit9@gmail.com>
2017-03-16 12:04:50 +0530

    Add Prescaling, Logistic regression

--- script.py
+++ script.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
+from sklearn.linear_model import logisticRegression
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+
 def simple_graphing(data):
 	positive = data.loc[data['Class'] == 1].values
 	negative = data.loc[data['Class'] == 0].values
@@ -28,6 +32,23 @@ if __name__ == '__main__':
 	#Uncomment to see simple graphs of the given data
 	#simple_graphing(data)		
 	data_sample = new_sample(data)
+	
+	#Separate input data
+	Y = data_sample['Class'].values
+	data_sample = data_sample.drop('Class', axis = 1)
+	X = data_sample.values
+	
+	#Scale input data
+	X = preprocessing.scale(X)
+	#Select val size
+	val_size = 0.1
+	
+	#Split Train and validation sets
+	X_train, X_val, y_train, y_test = train_test_split(X, Y, test_size = val_size)
+	#Using logistic regression
+	clf = LogisticRegression()
+	clf.fit(X_train, y_train)
+	
 	
 	
 	
